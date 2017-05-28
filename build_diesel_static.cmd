0a6d887d609815bcb9cb7229925d5ebd235ccecb
Jim McGrath <jimmc2@gmail.com>
2017-05-27 22:59:45 -0500

    allow rerunning without spurious errors

--- build_diesel_static.cmd
+++ build_diesel_static.cmd
@@ -1,12 +1,12 @@
 
 cd %~dp0
 
-git clone https://github.com/Microsoft/vcpkg
-git clone https://github.com/mcgoo/diesel
+if not exist vcpkg git clone https://github.com/Microsoft/vcpkg
+if not exist diesel git clone https://github.com/mcgoo/diesel
 
 rem Install database drivers in vcpkg
 set VCPKG_ROOT=%~dp0\vcpkg
-call %VCPKG_ROOT%\bootstrap-vcpkg.bat
+if not exist %VCPKG_ROOT%\vcpkg.exe call %VCPKG_ROOT%\bootstrap-vcpkg.bat
 %VCPKG_ROOT%\vcpkg install libmysql:x64-windows-static libpq:x64-windows-static sqlite3:x64-windows-static
 
 rem Build diesel
@@ -14,5 +14,5 @@ cd diesel
 set RUSTFLAGS=-Ctarget-feature=+crt-static
 git checkout vcpkg
 cd diesel_cli
-cargo run --no-default-features --features="postgres sqlite"
+cargo run
 
