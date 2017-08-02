32b1e46118f6f7cd581cff4ccd0e9d37adf911da
Jim McGrath <jmcgrath@hakafin.com>
2017-08-02 17:10:42 +0000

    exit with a failure code if anything fails

--- install_diesel_cli.cmd
+++ install_diesel_cli.cmd
@@ -1,4 +1,3 @@
-
 cd %~dp0
 
 if not exist vcpkg git clone https://github.com/Microsoft/vcpkg
@@ -6,8 +5,8 @@ if not exist vcpkg git clone https://github.com/Microsoft/vcpkg
 rem Install database drivers in vcpkg
 set VCPKG_ROOT=%~dp0\vcpkg
 if not exist %VCPKG_ROOT%\vcpkg.exe call %VCPKG_ROOT%\bootstrap-vcpkg.bat
-%VCPKG_ROOT%\vcpkg --triplet x64-windows-static install libmysql libpq sqlite3
+%VCPKG_ROOT%\vcpkg --triplet x64-windows-static install libmysql libpq sqlite3 || exit /b
 
 set RUSTFLAGS=-Ctarget-feature=+crt-static
-cargo install -f diesel_cli
+cargo install -f diesel_cli || exit /b
 pause
